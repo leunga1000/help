6f39bfe9f8f9b58d07682d49ee827c29ed7ebc2f
leunga1000 <24807638+leunga1000@users.noreply.github.com>
2025-05-22 18:22:42 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,169 @@
+import os
+import json
+from string import Template
+from datetime import datetime
+from aiohttp import web
+
+
+from collections import defaultdict 
+
+
+async def hello(request):
+    return web.Response(text=TEXT, content_type="text/html")
+
+
+INTRO_TEXT = """
+welcome to my antisocial counter.
+
+
+i believe i am the object of a cyberattack, that happened some time ago.
+
+to test my theory, this is an anonymous question/poll 
+
+"""
+
+TEXT = f""" 
+<html>
+<script>
+
+  function doclick() {{
+//
+}}
+
+</script>
+<pre>
+
+hi 
+
+{INTRO_TEXT}
+
+<!--<form action="/post" method="POST"> -->
+<form action="/post" method="POST"> 
+Question 1: Have you or your family/friends/work/network been approached, contacted 
+or interacted with by anyone claiming to be speaking on my or my network's behalf, 
+running a study, running an investigation, running surveillance, running probation, running an
+experiment, from the press, from the police, from the intelligence services, 
+whether this was directly or indirectly, outright or implied via contact physical, 
+electronic or online? 
+
+This may have been over the last 12 years.
+
+
+<input type=radio name="q1yes">Yes.</input>
+<input type=radio name="q1no">No. Absolutely not. No way. Never. How would it work?</input>
+
+
+Question 2: Do you suspect whether you have been the subject of such a cyberattack also.
+
+<input type=radio name="q2yes">Yes.</input>
+<input type=radio name="q2no">No. Absolutely not. No way. Never. How would it work?</input>
+
+
+If not, move on and don't look back!
+
+Optional:
+Message:
+<textarea name=message size=15 rows=5 cols=30> </textarea>
+<label> Show message to all <input type=checkbox name="message_public"></input> </label>
+<!--<input type=submit onclick="doclick();"></input>-->
+<input type=submit></input>
+</form>
+
+</pre>
+
+</html>
+"""
+
+
+def log_response(answers, date, message, message_public):
+    date_j = json.dumps(date)
+    answers_j = json.dumps(answers)
+    message_j = json.dumps(message)
+    message_public_j = json.dumps(message_public)
+     
+    template = Template("""
+          {"answers": $answers_j, "date": $date_j, "message": $message_j, "message_public": $message_public_j}
+    """)
+    result = template.substitute(answers_j=answers_j, date_j=date_j, message_j=message_j, message_public_j=message_public_j)
+
+    with open('data.log', 'a') as f:
+        f.write(result)
+
+def get_responses():
+    results = {}
+    #results["messages"] = []
+    full_results = [] 
+    log_name = 'data.log'
+
+    if not os.path.exists(log_name):
+        return {}, {}
+
+    for line in data.split("\n"):
+        line = line.strip()
+        if not line:
+            continue
+
+        try:
+            entry = json.loads(line)
+        except Exception as e:
+            print(f"parse error for {line}")
+            continue
+        
+        if entry.get("message"):
+            if entry.get("message_public"):
+                full_result = {"message": entry.get("message"), "date": entry.get("date")}
+            else:
+                full_result = {"message": "message private", "date": entry.get("date")}
+        else:
+            full_result = {"date": entry.get("date")} 
+            
+        
+        
+        print(entry.get('answers') )
+        for k, v in entry.get('answers', {}).items():
+            if k not in results:
+                results[k] = 1
+            else:
+                results[k] += 1
+            full_result[k] = v
+
+        full_results.append(full_result)
+
+    print(results)
+    return results, full_results
+    
+
+def render_responses():
+    r, fr = get_responses()        
+    t = Template("""
+    
+     {$r}
+
+     {$fr} 
+""")
+    return t.substitute(r=r, fr=fr)
+
+async def post_handler(request):
+    data = await request.post()
+    print(data)
+    # answer_cat = ["q1yes", "q1no", "q2yes", "q2no", "message"]
+    message = data.get('message', "")
+    message_public = data.get('message_public', "")
+    answers = {k: v for k, v in data.items() if k not in ("message", "message_public") }
+    date = str(datetime.now())
+    log_response(answers, date, message, message_public)
+    
+    return web.Response(text="""thanks for submitting. click <a href="/"> here </a> to go back to the main page""", content_type="text/html")
+
+
+app = web.Application()
+app.add_routes([web.get("/", hello), 
+    web.post('/post', post_handler),])
+
+#get_responses()
+print(render_responses())
+web.run_app(app)
+
+    with open('data.log', 'r') as f:
+        data = f.read()
+
